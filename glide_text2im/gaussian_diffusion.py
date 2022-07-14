d017696f9fc301c7314be4fea827ba06859ae40a
Penguin.jpg <sky026912@gmail.com>
2022-07-14 22:07:20 +0800

    update skip_timesteps and init_image functionality for ddim_sample_loop, change variable name 'out' in ddim_sample to 'out_orig'

--- glide_text2im/gaussian_diffusion.py
+++ glide_text2im/gaussian_diffusion.py
@@ -488,7 +488,7 @@ class GaussianDiffusion:
 
         Same usage as p_sample().
         """
-        out = self.p_mean_variance(
+        out_orig = self.p_mean_variance(
             model,
             x,
             t,
@@ -497,7 +497,7 @@ class GaussianDiffusion:
             model_kwargs=model_kwargs,
         )
         if cond_fn is not None:
-            out = self.condition_score(cond_fn, out, x, t, model_kwargs=model_kwargs)
+            out = self.condition_score(cond_fn, out_orig, x, t, model_kwargs=model_kwargs)
 
         # Usually our model outputs epsilon, but we re-derive it
         # in case we used x_start or x_prev prediction.
@@ -520,7 +520,7 @@ class GaussianDiffusion:
             (t != 0).float().view(-1, *([1] * (len(x.shape) - 1)))
         )  # no noise when t == 0
         sample = mean_pred + nonzero_mask * sigma * noise
-        return {"sample": sample, "pred_xstart": out["pred_xstart"]}
+        return {"sample": sample, "pred_xstart": out_orig["pred_xstart"]}
 
     def ddim_reverse_sample(
         self,
@@ -572,6 +572,8 @@ class GaussianDiffusion:
         device=None,
         progress=False,
         eta=0.0,
+        skip_timesteps=0,
+        init_image=None,
         randomize_class=False,
     ):
         """
@@ -591,6 +593,8 @@ class GaussianDiffusion:
             device=device,
             progress=progress,
             eta=eta,
+            skip_timesteps=skip_timesteps,
+            init_image=init_image,
             randomize_class=randomize_class,
         ):
             final = sample
@@ -608,6 +612,8 @@ class GaussianDiffusion:
         device=None,
         progress=False,
         eta=0.0,
+        skip_timesteps=0,
+        init_image=None,
         randomize_class=False,
     ):
         """
@@ -623,7 +629,16 @@ class GaussianDiffusion:
             img = noise
         else:
             img = th.randn(*shape, device=device)
-        indices = list(range(self.num_timesteps))[::-1]
+            
+        if skip_timesteps and init_image is None:
+            init_image = th.zeros_like(img)
+
+        indices = list(range(self.num_timesteps - skip_timesteps))[::-1]
+
+        if init_image is not None:
+            fac_1 = self.sqrt_alphas_cumprod[indices[0]]
+            fac_2 = self.sqrt_one_minus_alphas_cumprod[indices[0]]
+            img = init_image * fac_1 + img * fac_2
 
         if progress:
             # Lazy import so that we don't depend on tqdm.
